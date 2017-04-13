4630662248af46c57e8fd6fa12a971a4166e6808
Kyotense <makoto_singleton@hotmail.com>
2017-04-13 14:09:10 -0700

    playing around with css

--- Django-blog/mysite/personal/templatetags/extra.py
+++ Django-blog/mysite/personal/templatetags/extra.py
@@ -5,17 +5,17 @@ from games.models import VideoGame
 
 register = template.Library()
 
-@register.inclusion_tag("about/abouts.html")
+@register.inclusion_tag("about/about_snippet.html")
 def about_us():
     about_list = AboutUs.objects.all()
     return {'about_list':about_list}
 
-@register.inclusion_tag("blog/blog_carousel.html")
+@register.inclusion_tag("blog/blog_snippet.html")
 def blog_record():
     blog_list = Post.objects.all().order_by("-date")[:3]
     return {'blog_list':blog_list}
 
-@register.inclusion_tag("games/games_carousel.html")
+@register.inclusion_tag("games/games_snippet.html")
 def game_record():
     game_list = VideoGame.objects.all()
     return {'game_list':game_list}
